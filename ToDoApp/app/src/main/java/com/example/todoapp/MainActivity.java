d67737ba08b719a0b4f6797b33f4331ea01a9d51
Shreyash <shreyashpardhi1@gmail.com>
2023-04-30 20:27:47 +0530

    DataBase commit

--- ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.java
+++ ToDoApp/app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.todoapp.Utils.DataBaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+    private FloatingActionButton fbt;
+    private DataBaseHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.recyclerView);
+        fbt = findViewById(R.id.floBut);
+        myDB = new DataBaseHelper(MainActivity.this);
+
+        fbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
