0332ba064cd810fcc6e9830b9586bdb5f705f094
Shreyash <shreyashpardhi1@gmail.com>
2023-04-30 19:31:59 +0530

    Basic Structure

---
+++ ToDoApp/app/src/main/java/com/example/todoapp/startup_activity.java
@@ -0,0 +1,25 @@
+package com.example.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class startup_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(startup_activity.this, MainActivity.class));
+                finish();
+            }
+        } , 4000);
+    }
+}
